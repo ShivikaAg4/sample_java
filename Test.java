1e9daa8e10d9c20583cbf23f4be21f1324b4ee9c
ShivikaAg4 <124400595+ShivikaAg4@users.noreply.github.com>
2026-01-16 11:35:56 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,6 +6,7 @@ public class Test {
     int b=30;
     int result = a+b;
     System.out.println("The sum is " +result);
+    System.out.println("happy");
     
   }
   public static void main(String[] args)
